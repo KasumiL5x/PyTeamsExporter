709d2b17206270f6ae4fda4eef5ac5784507796e
KasumiL5x <KasumiL5x@gmail.com>
2021-09-11 11:41:47 +0100

    Major code cleanup.

--- app.py
+++ app.py
@@ -1,59 +1,66 @@
 import json
 import uuid
-import datetime
-from logging import exception
 import flask
 from flask.json import jsonify
-from oauthlib.oauth2.rfc6749.clients import base
 from requests_oauthlib import OAuth2Session
 
 from functools import wraps
 
-AUTHORITY_URL = 'https://login.microsoftonline.com/common'
-AUTH_ENDPOINT = '/oauth2/v2.0/authorize'
-TOKEN_ENDPOINT = '/oauth2/v2.0/token'
-CLIENT_ID = 'YOUR_CLIENT_ID_HERE'
+# Fill these in from your Azure app (see https://docs.microsoft.com/en-us/azure/active-directory/develop/quickstart-register-app).
+CLIENT_ID = 'YOUR_CLIENT_ID'
+CLIENT_SECRET = 'YOUR_CLIENT_SECRET'
 REDIRECT_URI = 'http://localhost:5000/login/authorized'
-CLIENT_SECRET = 'YOUR_CLIENT_SECRET_HERE'
 SCOPES = [
   "User.Read",
   "Chat.Read"
 ]
-
+# URLs and endpoints for authorization.
+AUTHORITY_URL = 'https://login.microsoftonline.com/common'
+AUTH_ENDPOINT = '/oauth2/v2.0/authorize'
+TOKEN_ENDPOINT = '/oauth2/v2.0/token'
+# Microsoft Graph API configuration.
 RESOURCE = 'https://graph.microsoft.com/'
 API_VERSION = 'beta' # NOTE: the 'beta' channel is required for the API calls we need to make.
 
 
+# Flask initialization.
 APP = flask.Flask(__name__, static_folder='static')
 APP.debug = True
 APP.secret_key = 'development'
 APP.config['SESSION_TYPE'] = 'filesystem'
-
 if APP.secret_key == 'development':
   import os
   os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'  # allows http requests
   os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'  # allows tokens to contain additional permissions
 
+# Create initial connection to Microsoft Graph.
 MSGRAPH = OAuth2Session(CLIENT_ID, redirect_uri=REDIRECT_URI, scope=SCOPES)
 
-# Render the homepage.
 @APP.route('/')
 def homepage():
+  """Render the homepage."""
+
+  # If user is already logged in, redirect them to the data page.
   if 'access_token' in flask.session:
-    return flask.redirect(flask.url_for('my_data'))
+    return flask.redirect('/mydata')
 
   return flask.render_template('index.html')
+#end
 
 @APP.route('/login')
 def login():
+  """Redirects the user to the Microsoft login page (which in turn redirects back to this app)."""
+
   flask.session.clear()
   auth_url, state = MSGRAPH.authorization_url(AUTHORITY_URL + AUTH_ENDPOINT)
   flask.session['state'] = state
-  print(auth_url)
   return flask.redirect(auth_url)
+#end
 
 @APP.route('/login/authorized')
 def authorized():
+  """Obtains and stores the access token for an authorized login."""
+
   if flask.session.get('state') and str(flask.session['state']) != str(flask.request.args.get('state')):
     raise Exception('state returned to redirect URL does not match!')
   
@@ -62,12 +69,22 @@ def authorized():
     client_secret=CLIENT_SECRET,
     authorization_response=flask.request.url
   )
-
   flask.session['access_token'] = token
 
   return flask.redirect('/')
+#end
+
+@APP.route('/logout')
+def logout():
+  """Does what it says on the tin."""
+
+  flask.session.clear()
+  return flask.redirect('/')
+#end
 
 def requires_auth(f):
+  """Defines a decorator so that functions can require an authorized user to work."""
+
   @wraps(f)
   def decorated(*args, **kwargs):
     if 'access_token' not in flask.session:
@@ -75,70 +92,72 @@ def requires_auth(f):
     if not MSGRAPH.authorized:
       return flask.redirect('/login')
     return f(*args, **kwargs)
+  #end
   return decorated
+#end
 
 @APP.route('/mydata')
 @requires_auth
 def my_data():
+  """Renders the 'my data' page if the user is logged in."""
+
+  # https://docs.microsoft.com/en-us/graph/api/user-get?view=graph-rest-beta&tabs=http
   base_url = RESOURCE + API_VERSION + '/'
   user_profile = MSGRAPH.get(base_url + 'me', headers=request_headers()).json()
+  # If the request gives an error, try to get the user to login again.
   if 'error' in user_profile:
     return flask.redirect('/login')
 
   username = user_profile['displayName']
   email = user_profile['userPrincipalName']
   return flask.render_template('mydata.html', username=username, email=email)
+#end
 
-@APP.route('/get_all_chats')
+@APP.route('/get_all_chats', methods=['GET'])
 @requires_auth
 def get_all_chats():
+  """Returns a JSON dictionary of all chats and associated metadata. This is NOT messages, but the chats themselves."""
+  # NOTE: See `all_chats` below for the structure of the returned JSON.
+
+  # https://docs.microsoft.com/en-us/graph/api/chat-list?view=graph-rest-beta&tabs=http
   base_url = RESOURCE + API_VERSION + '/'
   raw_chats = MSGRAPH.get(base_url + 'me/chats?$expand=members', headers=request_headers()).json()
 
   all_chats = []
 
   if 'value' in raw_chats:
-    index = 0
     total_chats = len(raw_chats['value'])
     print(f'Total chats: {total_chats}')
     for chat in raw_chats['value']:
       chat_id = chat['id']
-      chat_index = index+1
       chat_topic = chat['topic']
       chat_type = chat['chatType']
       chat_link = chat['webUrl']
 
-      total_members = len(chat['members'])
-      chat_members = f'({total_members})'
+      # Concatenate members with a maximum number.
+      members_data = chat['members']
+      total_members = len(members_data)
       max_members = 4
-      processed_members = 0
-      for member in chat['members']:
-        member_name = member['displayName']
-        chat_members += f' {member_name}'
-
-        if processed_members != total_members-1:
-          chat_members += ','
-
-        processed_members += 1
-        if processed_members >= max_members:
-          break
-      if processed_members != total_members:
-        chat_members += ' ...'
+      members = [x['displayName'] for x in members_data[:min(total_members, max_members)]]
+      members_str = ', '.join(members)
+      if total_members > max_members:
+        members_str += ', ...'
       
       all_chats.append({
         'id': chat_id,
-        'index': chat_index,
         'topic': chat_topic,
         'type': chat_type,
-        'members': chat_members,
+        'members': members_str,
         'link': chat_link
       })
-      
-      index += 1
+    #end for
+  #end if
 
   return flask.Response(json.dumps(all_chats), mimetype='application/json')
+#end
 
 def json_to_html_chat(data):
+  """Takes a data array from within `get_chat` and converts it into a pretty HTML document."""
   html_string = ''
 
   chat_data = data['chat']
@@ -153,7 +172,7 @@ def json_to_html_chat(data):
 
   messages_data = data['messages']
 
-  # Header
+  # Header.
   html_string += "<!DOCTYPE html>\n"
   html_string += "<html>\n"
   html_string += "<head>\n"
@@ -195,7 +214,7 @@ def json_to_html_chat(data):
   html_string += "\t\t</div>\n"
   html_string += "\t</div>\n"
 
-  # Messages
+  # Messages.
   html_string += "\t<ul class=\"list-group\">\n"
   for msg in messages_data:
     msg_from = msg['from']
@@ -210,107 +229,125 @@ def json_to_html_chat(data):
     html_string += f"\t\t\t<span class=\"badge bg-primary rounded-pill\">{msg_timestamp}</span>\n"
     html_string += "\t\t</li>\n"
     html_string += "\t</ul>\n\n"
+  #end
 
-  # Footer
+  # Footer.
   html_string += "\t<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ\" crossorigin=\"anonymous\"></script>\n"
   html_string += "</body>\n"
   html_string += "</html>"
 
   return html_string
+#end
 
 @APP.route('/get_chat', methods=['POST'])
 @requires_auth
 def get_chat():
-  if not flask.request.is_json:
-    return jsonify(message='Error. Input was not JSON.'), 400
+  """
+  Given a chat id, retrieves chat metadata and all messages. This is stored in JSON and pretty HTML
+  on the server, and the generated HTML file is then sent back to the browser as a file to download.
+  """
 
+  # Broadly sanity check input data type.
+  if not flask.request.is_json:
+    return jsonify(message='Input was not JSON.'), 400
+
+  # Make sure the appropriate JSON entry is present.
   chat_id_key = 'chat_id'
   if chat_id_key not in flask.request.json:
-    return jsonify(message='Error. Failed to find chat_id entry.'), 400
+    return jsonify(message='Failed to find chat_id entry.'), 400
   
+  # Make sure the passed in value actually has data.
   chat_id = flask.request.json[chat_id_key]
   if not len(chat_id):
-    return jsonify(message='Error: chat_id key was empty.'), 400
+    return jsonify(message='chat_id value was empty.'), 400
 
-  # Get and build chat metadata.
+  # https://docs.microsoft.com/en-us/graph/api/chat-get?view=graph-rest-beta&tabs=http
   chat_base_url = RESOURCE + API_VERSION + '/'
   raw_chat = MSGRAPH.get(chat_base_url + f'/me/chats/{chat_id}?$expand=members', headers=request_headers()).json()
+  # Double check that we can get the chat data (i.e., is the ID correct and does it return valid data).
   if 'error' in raw_chat:
-    return jsonify(message='Error: Unable to retrieve chat metadata.'), 400
-  chat_data = {
-    'topic': 'Unnamed Chat' if raw_chat['topic'] is None else raw_chat['topic'],
-    'type': raw_chat['chatType'],
-    'when': raw_chat['createdDateTime'],
-    'link': raw_chat['webUrl']
-  }
+    return jsonify(message='Unable to retrieve chat metadata.'), 400
+  
+  # Build chat metadata dictionary.
   chat_members = []
   for member in raw_chat['members']:
     member_name = member['displayName']
     member_email = member['email']
     chat_members.append(f'{member_name} ({member_email})')
-  chat_data['members'] = chat_members
+  #end
+  chat_data = {
+    'topic': 'Unnamed Chat' if raw_chat['topic'] is None else raw_chat['topic'],
+    'type': raw_chat['chatType'],
+    'when': raw_chat['createdDateTime'],
+    'link': raw_chat['webUrl'],
+    'members': chat_members
+  }
 
+  # https://docs.microsoft.com/en-us/graph/api/chat-list-messages?view=graph-rest-beta&tabs=http
   base_url = RESOURCE + API_VERSION + '/'
   raw_messages = MSGRAPH.get(base_url + f'me/chats/{chat_id}/messages', headers=request_headers()).json()
 
+  # Double check that actual message data was received.
+  if 'error' in raw_messages:
+    return jsonify(message='Unable to retrieve chat messages.'), 400
+
   all_messages = []
-  if 'value' in raw_messages:
-    index = 0
-    total_chats = len(raw_messages['value'])
-    print(f'Total messages: {total_chats}')
-    for msg in raw_messages['value']:
-      # Only process user messages... for now.
-      if msg['messageType'] != 'message':
-        continue
+  for msg in raw_messages['value']:
+    # Only process user messages... for now.
+    if msg['messageType'] != 'message':
+      continue
 
-      msg_entry = {}
+    msg_entry = {}
+    msg_entry['from'] = msg['from']['user']['displayName']
+    msg_entry['when'] = msg['createdDateTime']
+    msg_entry['type'] = msg['body']['contentType']
+    # Only handle certain types of content for now.
+    if msg['body']['contentType'] in ['text', 'html']:
+      msg_entry['content'] = msg['body']['content']
+    else:
+      msg_entry['content'] = ''
 
-      msg_entry['from'] = msg['from']['user']['displayName']
-      msg_entry['when'] = msg['createdDateTime']
-
-      msg_entry['type'] = msg['body']['contentType']
-
-      # Only handle certain types of content.
-      if msg['body']['contentType'] in ['text', 'html']:
-        msg_entry['content'] = msg['body']['content']
-      else:
-        msg_entry['content'] = ''
-      
-      # TODO: Consider attachments.
-      # https://docs.microsoft.com/en-us/graph/api/attachment-get?view=graph-rest-beta&tabs=http
-      # Maybe I can have them downloaded in advance then inserted in with a link or something?
-
-      all_messages.append(msg_entry)
-    #end for
-  #end if
+    all_messages.append(msg_entry)
+    
+    # TODO: Consider attachments.
+    # https://docs.microsoft.com/en-us/graph/api/attachment-get?view=graph-rest-beta&tabs=http
+    # Maybe I can have them downloaded in advance then inserted in with a link or something?
+    # Perhaps I can download them all, zip them up, then send the zip back?
+    # Perhaps this is a separate button in the UI in each row?
+    # Need to think about this. Messages for now, as those are the focus.
+  #end for
 
   # If no messages are present, return an error.
   if not len(all_messages):
-    return jsonify(message='Error: No valid messages found in chat.'), 400
+    return jsonify(message='No valid messages found in chat.'), 400
 
+  # Concatenate the final data that will be converted and saved out.
   final_data = {
     'chat': chat_data,
     'messages': all_messages
   }
 
+  # TODO: Come up with a more intelligent filename than a random uuid.
   random_filename = str(uuid.uuid4())
+
+  # Write out the dictionary to a raw JSON file.
   with open('static/files/' + random_filename + '.json', 'w+', encoding="utf-8") as out_file:
     out_file.write(json.dumps(final_data))
 
+  # Convert the dictionary into a pretty HTML page and write that out.
   with open('static/files/' + random_filename + '.html', 'w+', encoding="utf-8") as out_file:
     out_file.write(json_to_html_chat(final_data))
   
+  # Return the HTML file from disk.
   return flask.send_file('static/files/' + random_filename + '.html', as_attachment=True)
+
+  # NOTE: I read online that this is safer and I shouldn't use `send_file`. Will look into it later.
   # return flask.send_from_directory('static/files', random_filename, as_attachment=True, max_age=0)
-  # return flask.Response(result_str, mimetype='application/json')
+#end
 
-@APP.route('/logout')
-def logout():
-  flask.session.clear()
-  return flask.redirect('/')
-
-# Returns a dictionary of default HTTP headers for Graph API calls.
 def request_headers(headers=None):
+  """Returns a dictionary of default HTTP headers for Graph API calls."""
+
   default_headers = {
     'SdkVersion': 'sample-python-flask',
     'x-client-SKU': 'sample-python-flask',
@@ -320,6 +357,7 @@ def request_headers(headers=None):
   if headers:
     default_headers.update(headers)
   return default_headers
+#end
 
 if __name__ == '__main__':
   APP.run()
